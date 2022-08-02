0c49bbd097820dc18a7cc20507ec4e265aa513eb
chayan0619 <52231215+chayan0619@users.noreply.github.com>
2022-08-02 14:26:45 +0530

    Update settings.py

--- MAC/settings.py
+++ MAC/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1e5owdw%a&3&_72ge(i$q$8#!9=r!$0&&cg0b3-hv-+ni!y_1o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
